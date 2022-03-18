b86dbf998fb9b98b7b8012501959e63f883f82ab
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-03-18 09:51:37 +0530

    mongodb collection established

--- mongo.js
+++ mongo.js
@@ -1,9 +1,23 @@
 import { MongoClient } from "mongodb";
+const mongoUrl = "mongodb://localhost:27017";
+const mongoDbName = "student-data";
 
-const mongo={
-    db:null,
 
-    async connect(){
+const mongo = {
+    db: null, //DB connection string 
+    batch: null, // batch collection 
+    student: null, //student collection 
 
-    }
-}
+    async connect() {
+        const client = new MongoClient(mongoUrl);
+        await client.connect();
+        console.log(`Mongo db connected-${mongoUrl}`);
+        this.db = client.db(mongoDbName);
+        console.log(`MongoDB selected -${mongoDbName}`);
+
+        this.batch = this.db.collection("batch");
+        this.student = this.db.collection("student");
+        console.log("collections initialized ")
+
+    },
+};
